319352a7a33cbd59002e7558400bc01e01f4acb6
chennan <327919006@qq.com>
2023-04-29 22:31:51 +0800

    整合元数据传递

---
+++ order-service/src/main/java/com/cn/polaris/sample/order/config/CustomMetadata.java
@@ -0,0 +1,47 @@
+package com.cn.polaris.sample.order.config;
+
+import com.tencent.cloud.common.spi.InstanceMetadataProvider;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Chen Nan
+ */
+@Component
+public class CustomMetadata implements InstanceMetadataProvider {
+
+    @Override
+    public Map<String, String> getMetadata() {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("custom_key1", "data");
+        return metadata;
+    }
+
+    @Override
+    public Set<String> getTransitiveMetadataKeys() {
+        return null;
+    }
+
+    @Override
+    public Set<String> getDisposableMetadataKeys() {
+        return null;
+    }
+
+    @Override
+    public String getRegion() {
+        return null;
+    }
+
+    @Override
+    public String getZone() {
+        return null;
+    }
+
+    @Override
+    public String getCampus() {
+        return null;
+    }
+}
